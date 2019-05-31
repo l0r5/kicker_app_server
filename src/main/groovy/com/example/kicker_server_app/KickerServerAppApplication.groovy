e38ff1b21d482ab61c46f8a9ed3d20d5bbb40456
Lars <lars.braitinger@gmail.com>
2019-05-31 18:04:16 +0200

    set up initial spring boot

---
+++ src/main/groovy/com/example/kicker_server_app/KickerServerAppApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.kicker_server_app
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KickerServerAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(KickerServerAppApplication, args)
+	}
+
+}
